d6656e132434c7c371da133aa82a5862744095f8
Boluwatife Fakorede <bfakorede@avallain.com>
2019-09-16 17:13:14 +0100

    Creating a simple Unary API

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -11,6 +12,20 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v \n", req)
+
+	firstName := req.GetGreeting().GetFirstName()
+
+	result := "hello " + firstName
+
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
+}
 
 func main() {
 	fmt.Println("HELLO WORLD")
